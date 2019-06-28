a82b28db47e77b860034750d37441454e2bea240
GodFlowDix <mdixnpnf@gmail.com>
2019-06-27 23:31:22 -0400

    Swapping through screens programatticaly
    This is a simple app where I practiced navigating through screens without a storyboard.

--- ProgramUI/AppDelegate.swift
+++ ProgramUI/AppDelegate.swift
@@ -16,7 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let firstScreen = FirstScreen()  //created first screen which is the red screen(seeFirstScreen.swift)
+        let navigationController = UINavigationController(rootViewController: firstScreen) //initialize nav controller with rootView Controller of the first Screen
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navigationController  //set the window of the rootview to navigationController
+        window?.makeKeyAndVisible()
+
         return true
     }
 
---
+++ ProgramUI/SecondScreen.swift
@@ -0,0 +1,23 @@
+//
+//  SecondScreen.swift
+//  ProgramUI
+//
+//  Created by Mike Dix on 6/27/19.
+//  Copyright © 2019 Mike Dix. All rights reserved.
+//
+
+import UIKit
+
+class SecondScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .darkGray
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
